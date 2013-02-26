a779f2da2a015435ffc6b81ab5b354e9a94fbaca
Thomas Gamble <thomasgamble@gokaconsulting.com>
2013-02-26 09:11:31 -0500

    Added logging to purgeservlet and purgeservice Added try/catch block to purgeservice

--- notifyweb/src/com/gokaconsulting/notifyweb/service/PurgeService.java
+++ notifyweb/src/com/gokaconsulting/notifyweb/service/PurgeService.java
@@ -2,6 +2,8 @@ package com.gokaconsulting.notifyweb.service;
 
 import java.util.Calendar;
 import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.jdo.PersistenceManager;
 import javax.jdo.Query;
@@ -10,27 +12,33 @@ import com.gokaconsulting.notifyweb.dao.PMF;
 import com.gokaconsulting.notifyweb.model.Notification;
 
 public class PurgeService {
+
+	private static final Logger logger = Logger.getLogger(PurgeService.class.getName());
 	
-	
-	public PurgeService()
-	{
-		
+	public PurgeService() {
+
 	}
-	
-	public void purge()
-	{
+
+	public void purge() {
 		PersistenceManager pm = PMF.get().getPersistenceManager();
-		
-		Date dateParam = new Date();
-		Calendar c = Calendar.getInstance(); 
-		c.setTime(dateParam); 
-		c.add(Calendar.DATE, -1);
-		dateParam = c.getTime();
-		
-		Query q = pm.newQuery(Notification.class);
-		q.setFilter("sentDate < dateParam");
-		q.declareImports("import java.util.Date");
-		q.declareParameters("Date dateParam");
-		q.deletePersistentAll(dateParam);
+		logger.info("Beginning purge process");
+		try {
+			Date dateParam = new Date();
+			Calendar c = Calendar.getInstance();
+			c.setTime(dateParam);
+			c.add(Calendar.DATE, -1);
+			dateParam = c.getTime();
+
+			Query q = pm.newQuery(Notification.class);
+			q.setFilter("sentDate < dateParam");
+			q.declareImports("import java.util.Date");
+			q.declareParameters("Date dateParam");
+			q.deletePersistentAll(dateParam);
+		} catch (Exception e) {
+			logger.log(Level.SEVERE, "Error purging old notifications", e);
+		} finally {
+			logger.warning("Purge process completed");
+			pm.close();
+		}
 	}
 }
