0a634cfd2c66c55f7822596b56480ee030ddbd35
Thomas Gamble <thomasgamble@gokaconsulting.com>
2013-03-05 11:47:07 -0500

    Adjusted pushnotification to not send notifications to users with more than 50 unread messages

--- notifyweb/src/com/gokaconsulting/notifyweb/gateway/AlertGateway.java
+++ notifyweb/src/com/gokaconsulting/notifyweb/gateway/AlertGateway.java
@@ -19,74 +19,75 @@ import com.google.gson.Gson;
 
 public class AlertGateway {
 
-	private static final Logger logger = Logger.getLogger(AlertGateway.class.getName());
+	private static final Logger logger = Logger.getLogger(AlertGateway.class
+			.getName());
 
-	public AlertGateway()
-	{
-		
+	public AlertGateway() {
+
 	}
-	
-	private String getAuthString(Boolean isValidated, String userName)
-	{
-		if(isValidated == null || !isValidated)
-		{
+
+	private String getAuthString(Boolean isValidated, String userName) {
+		if (isValidated == null || !isValidated) {
 			logger.warning("Sending alert to non-validated user: " + userName);
 			return Constants.getDevAuthString();
-		}
-		else
-		{
+		} else {
 			return Constants.getAuthString();
 		}
 	}
-	
+
 	public void sendAlert(Notification n, int unRead) {
 		PersistenceManager pm = PMF.get().getPersistenceManager();
 		try {
-			User user = pm.getObjectById(User.class, n.getUserEmail());
-			if (user != null) {
+			if (unRead < 50) {
 
-				URL url = new URL(Constants.URBAN_AIRSHIP_API);
-				HttpURLConnection connection = (HttpURLConnection) url
-						.openConnection();
-				connection.setRequestMethod("POST");
-				connection.setDoOutput(true);
+				User user = pm.getObjectById(User.class, n.getUserEmail());
+				if (user != null) {
 
-				String authString = this.getAuthString(user.isValidated(), user.getUserAddress());
-				
-				String authStringBase64 = Base64.encodeBase64String(authString
-						.getBytes());
-				authStringBase64 = authStringBase64.trim();
+					URL url = new URL(Constants.URBAN_AIRSHIP_API);
+					HttpURLConnection connection = (HttpURLConnection) url
+							.openConnection();
+					connection.setRequestMethod("POST");
+					connection.setDoOutput(true);
 
-				connection.setRequestProperty("Content-type",
-						"application/json");
-				connection.setRequestProperty("Authorization", "Basic "
-						+ authStringBase64);
+					String authString = this.getAuthString(user.isValidated(),
+							user.getUserAddress());
 
-				Gson gson = new Gson();
-				OutputStreamWriter osw = new OutputStreamWriter(
-						connection.getOutputStream());
-				gson.toJson(
-						new PushNotification(n.getUserEmail(), n
-								.getFromAddress() + ": " + n.getSubject(),
-								"default", unRead), osw);
-				// osw.write("{\"aps\":{\"alert\":\"New Mail!\", \"sound\": \"default\"}, \"aliases\": [\"thomas_gamble@homedepot.com\"]}");
-				osw.close();
+					String authStringBase64 = Base64
+							.encodeBase64String(authString.getBytes());
+					authStringBase64 = authStringBase64.trim();
 
-				int responseCode = connection.getResponseCode();
-				logger.info("Notification submitted with response code: "
-						+ responseCode);
-				// logger.info(authString);
-				logger.info(gson.toJson(
-						new PushNotification(n.getUserEmail(), n
-								.getFromAddress() + ": " + n.getSubject(),
-								"default", unRead)));
-			}
-			else {
-				logger.info("User passed to notification was null");
+					connection.setRequestProperty("Content-type",
+							"application/json");
+					connection.setRequestProperty("Authorization", "Basic "
+							+ authStringBase64);
+
+					Gson gson = new Gson();
+					OutputStreamWriter osw = new OutputStreamWriter(
+							connection.getOutputStream());
+					gson.toJson(
+							new PushNotification(n.getUserEmail(), n
+									.getFromAddress() + ": " + n.getSubject(),
+									"default", unRead), osw);
+					// osw.write("{\"aps\":{\"alert\":\"New Mail!\", \"sound\": \"default\"}, \"aliases\": [\"thomas_gamble@homedepot.com\"]}");
+					osw.close();
+
+					int responseCode = connection.getResponseCode();
+					logger.info("Notification submitted with response code: "
+							+ responseCode);
+					// logger.info(authString);
+					logger.info(gson.toJson(new PushNotification(n
+							.getUserEmail(), n.getFromAddress() + ": "
+							+ n.getSubject(), "default", unRead)));
+				} else {
+					logger.info("User passed to notification was null");
+				}
+			} else {
+				logger.warning(n.getUserEmail() + " has " + unRead
+						+ " unread messages, notification not sent");
 			}
 		} catch (Exception e) {
 			logger.log(Level.SEVERE, "Error sending notification", e);
 		}
 	}
-	
+
 }
